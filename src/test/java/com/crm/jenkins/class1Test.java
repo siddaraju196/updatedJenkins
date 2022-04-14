fa6898aabeafb1252ed6c4be71f73f1284e3ca72
siddaraju196 <91187173+siddaraju196@users.noreply.github.com>
2022-04-14 18:05:00 +0530

    des

--- src/test/java/com/crm/jenkins/class1Test.java
+++ src/test/java/com/crm/jenkins/class1Test.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class class1Test {
 	@Test
 	public void test() {
-		System.out.println("system test1");
+		System.out.println("system test1 to kgf");
 	}
 
 }
